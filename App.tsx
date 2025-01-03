49a1dd7fb52883d93a6d4b8fd4d1d9b0fd83440b
Rami Elwan <ramielwan48@gmail.com>
2025-01-03 12:33:50 +0100

    refactor: call method on app mount

--- App.tsx
+++ App.tsx
@@ -1,43 +1,48 @@
+import {useEffect} from 'react';
 import {Button, View} from 'react-native';
 import * as RNIap from 'react-native-iap';
 
 RNIap.setup({storekitMode: 'STOREKIT2_MODE'});
 
+async function logSubscriptionsDetails() {
+  let activeSubscriptionsCount = 0;
+
+  // Fetch the product, otherwise `getAvailablePurchases` will return an empty array
+  await RNIap.getProducts({skus: ['premium_one_month', 'premium_six_months']});
+
+  for (const purchase of await RNIap.getAvailablePurchases({
+    onlyIncludeActiveItems: true,
+  })) {
+    const entitledPurchase = await RNIap.IapIosSk2.currentEntitlement(purchase.productId);
+    const expirationDate = new Date(entitledPurchase.expirationDate);
+
+    const subscriptionStatus = await RNIap.IapIosSk2.subscriptionStatus(purchase.productId);
+    const isSubscribed = subscriptionStatus.some(status => status.state === 'subscribed');
+
+    if (isSubscribed && expirationDate.getTime() > Date.now()) {
+      activeSubscriptionsCount++;
+      logSubscriptionDetails(purchase.productId, expirationDate, entitledPurchase.offerID);
+    }
+  }
+
+  if (activeSubscriptionsCount === 0) {
+    console.log('User has no active subscriptions');
+  }
+}
+
+function logSubscriptionDetails(productId: string, expirationDate: Date, offerId?: string) {
+  console.log(`User has an active subscription for ${productId}`);
+  console.log(`Subscription will expire at ${expirationDate.toLocaleString()}`);
+
+  if (typeof offerId === 'string') {
+    console.log(`User subscribed with offer ID ${offerId}`);
+  }
+}
+
 function App() {
-  async function logSubscriptionsDetails() {
-    let activeSubscriptionsCount = 0;
-
-    // Fetch the product, otherwise `getAvailablePurchases` will return an empty array
-    await RNIap.getProducts({skus: ['premium_one_month', 'premium_six_months']});
-
-    for (const purchase of await RNIap.getAvailablePurchases({
-      onlyIncludeActiveItems: true,
-    })) {
-      const entitledPurchase = await RNIap.IapIosSk2.currentEntitlement(purchase.productId);
-      const expirationDate = new Date(entitledPurchase.expirationDate);
-
-      const subscriptionStatus = await RNIap.IapIosSk2.subscriptionStatus(purchase.productId);
-      const isSubscribed = subscriptionStatus.some(status => status.state === 'subscribed');
-
-      if (isSubscribed && expirationDate.getTime() > Date.now()) {
-        activeSubscriptionsCount++;
-        logSubscriptionDetails(purchase.productId, expirationDate, entitledPurchase.offerID);
-      }
-    }
-
-    if (activeSubscriptionsCount === 0) {
-      console.log('User has no active subscriptions');
-    }
-  }
-
-  function logSubscriptionDetails(productId: string, expirationDate: Date, offerId?: string) {
-    console.log(`User has an active subscription for ${productId}`);
-    console.log(`Subscription will expire at ${expirationDate.toLocaleString()}`);
-
-    if (typeof offerId === 'string') {
-      console.log(`User subscribed with offer ID ${offerId}`);
-    }
-  }
+  useEffect(() => {
+    logSubscriptionsDetails();
+  }, []);
 
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
